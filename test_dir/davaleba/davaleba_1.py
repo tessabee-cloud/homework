8af0d6b681f75ebf98ab2ad818ad46e5cdc71e7c
tessabee-cloud <ana.baghaturia@gmail.com>
2026-04-23 21:48:43 +0400

    1st commit

---
+++ test_dir/davaleba/davaleba_1.py
@@ -0,0 +1,24 @@
+
+
+#1
+num_1 = 5
+num_2 = 6.5
+num_3 = 'test'
+num_4 = 5 > 4
+
+print(type(num_1))
+print(type(num_2))
+print(type(num_3))
+print(type(num_4))
+
+#2
+num1 = int(input('input your birth year '))
+num2 = 2025
+
+print(num2 - num1)
+
+#3
+print(24 > 0)
+print(24 < 0)
+print(24 == 0)
+print(24 % 2)
